fd0c22fc855b0dfc771a544e27547a0dd8c60bda
Vikranth V <vvikranth08@gmail.com>
2025-09-17 20:04:00 +0530

    Add files via upload

---
+++ 6-(i)_Insertion_sorting_implementation_in_array.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+void insertionSort(int arr[], int n) {
+    int i, j, key;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+int main() {
+    int n, i;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    insertionSort(arr, n);
+    printf("Sorted array: ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
+
+/* output:
+Enter number of elements: 5
+Enter 5 elements:
+64 25 12 22 11
+Sorted array: 11 12 22 25 64
+*/
